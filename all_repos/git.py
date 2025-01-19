490065b620153918a09f41737e4c17a6c9aa90c2
Rasmus Nygren <rasmus.nygren99@gmail.com>
2025-01-19 10:50:58 +0100

    add repo_name helper

--- all_repos/git.py
+++ all_repos/git.py
@@ -7,3 +7,7 @@ def remote(path: str) -> str:
     return subprocess.check_output((
         'git', '-C', path, 'config', 'remote.origin.url',
     )).decode().strip()
+
+
+def repo_name(path: str) -> str:
+    return remote(path).split("/")[-1].split(".git")[0]
